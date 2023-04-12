638eb64cbcc7de49797260088d7908eefe431417
啊实打实多 <358524227@qq.com>
2023-04-12 13:57:37 +0800

    統一返回

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,21 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function success($msg,$data = [])
+    {
+        return response()->json([
+            'status' => 1,
+            'info' => $msg,
+            'result' => $data,
+        ]);
+    }
+
+    public function error($msg,$data){
+        return response()->json([
+            'status' => 0,
+            'info' => $msg,
+            'result' => $data,
+        ]);
+    }
 }
