ba8f1b749667b343924573df544b38656ed24ec2
nicework <aaaaa@gmail.com>
2023-04-12 15:46:22 +0800

    后台路由

--- app/Http/Controllers/admin/LoginController.php
+++ app/Http/Controllers/admin/LoginController.php
@@ -14,6 +14,7 @@ class LoginController extends Controller
 {
     public function login(Request $request)
     {
+        var_dump(env("DB_PASSWORD"));
         $username = $request->input("username");
         $password = $request->input("password");
         $adminUser = AdminUser::where('username', $username)->first();
