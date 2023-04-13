f5a35424418eba954584abf75cf5f3b53b88a6e6
nicework <aaaaa@gmail.com>
2023-04-13 18:54:12 +0800

    后台路由

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -5,33 +5,24 @@ namespace App\Http\Controllers\Admin;
 use App\Common\Rsa;
 use App\Http\Controllers\BaseController;
 use App\Models\AdminUser;
+use App\Validate\Admin\AdminUserValidate;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class LoginController extends BaseController
 {
+    private $validate;
+    public function __construct(AdminUserValidate $validate)
+    {
+        $this->validate = $validate;
+    }
+
     public function login(Request $request)
     {
-
-//        $request->validate([
-//            'username' => 'required|string',
-//            'password' => 'required|string',
-//        ]);
         $credentials = $request->only('username', 'password');
-//        $username = $request->input("username");
-//        $password = $request->input("password");
-//
-//        $token = auth()->tokenById(1);
-//        var_dump($token);
-//        var_dump(auth()->validate());
-//        $adminUser = AdminUser::where('username', $username)->first();
-//        if ($adminUser == null) {
-//            return $this->fail(0, "账号不存在");
-//        }
-//        if (Rsa::encryptPass($password, $adminUser->salt) != $adminUser->password) {
-//            return $this->error("密码");
-//        }
-//        return $credentials;
+        if(!$this->validate->scene('login')->check($credentials)){
+            return $this->fail($this->validate->getError());
+        }
         $token = auth('admin')->attempt($credentials);
         if (!$token) {
             return $this->fail('登录失败');
