52a9a09ac6667bebe65157fab0233ebbfe1a0ab0
nicework <aaaaa@gmail.com>
2023-04-13 20:35:50 +0800

    后台路由

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\AdminNav;
+use App\Models\AdminGroup;
+use App\Models\AdminRule;
 use App\Validate\Admin\AdminUserValidate;
 use Illuminate\Http\Request;
 
@@ -9,6 +12,7 @@ use Illuminate\Http\Request;
 class LoginController extends AdminBaseController
 {
     private $validate;
+
     public function __construct(AdminUserValidate $validate)
     {
         $this->validate = $validate;
@@ -18,7 +22,7 @@ class LoginController extends AdminBaseController
     {
         $credentials = $request->only('username', 'password');
 
-        if(!$this->validate->scene('login')->check($credentials)){
+        if (!$this->validate->scene('login')->check($credentials)) {
             return $this->fail($this->validate->getError());
         }
         $token = auth('admin')->setTTl(10)->attempt($credentials);
@@ -27,13 +31,35 @@ class LoginController extends AdminBaseController
         }
 
         $user = auth('admin')->user();
-        return $this->success('登录成功',[
-            'user'=>$user,
+        return $this->success('登录成功', [
+            'user' => $user,
         ]);
     }
 
     public function menuList(Request $request)
     {
-
+        $admin = auth("admin")->user();
+        $ag = AdminGroup::where("group_id", $admin->group_id);
+        $menuOne = AdminNav::where("pid", 0)->orderBy("order_number", "desc")->select("id,title,icon,path")->get()->toArray();
+        $rules = explode(",", $ag->rules);
+        foreach ($menuOne as $k => &$v) {
+            $exist = AdminRule::where("nav_id", $v["id"])->where("id", "in", $rules)->first();
+            $menuTwo = AdminNav::where("pid", $v["id"])->select("id,title,icon,path")->get()->toArray();
+            foreach ($menuTwo as $a => &$m) {
+                $existTwo = AdminRule::where("nav_id", $m["id"])->where("id", "in", $rules)->first();
+                if (!$existTwo) {
+                    unset($menuTwo[$a]);
+                }
+            }
+            if (!$exist && empty($menuTwo)) {
+                unset($menuOne[$k]);
+            }
+            if (!empty($menuTwo)) {
+                $menuTwo = array_values($menuTwo);
+                $v["children"] = $menuTwo;
+            }
+        }
+        $menuOne = array_values($menuOne);
+        return $this->success("请求成功", $menuOne);
     }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -4,8 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\LoginController;
 
 Route::middleware(['admin.sign'])->prefix("hack")->group(function () {
-    Route::controller(LoginController::class)->group(function () {
-        Route::post("login", 'login');
+    Route::post("login",[LoginController::class,"login"]);
+    Route::middleware(['admin.token'])->group(function (){
+        Route::controller(LoginController::class)->group(function () {
+            Route::post("menuList", 'menuList');
+        });
     });
 });
 
