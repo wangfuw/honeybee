c206b8491a5e880f9b3be07bdeca45fddd59c51a
nicework <aaaaa@gmail.com>
2023-04-13 16:12:18 +0800

    后台路由

--- routes/admin.php
+++ routes/admin.php
@@ -1,11 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\LoginController;
-use App\Http\Middleware\AdminSign;
 
-Route::middleware(['middleware' => 'admin.sign'])->group(function () {
+Route::middleware(['admin.sign'])->prefix("/hack")->group(function () {
     Route::controller(LoginController::class)->group(function () {
         Route::post("login", 'login');
     });
