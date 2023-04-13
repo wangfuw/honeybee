d8ea570e7cc14ef774e2a6b9dd13cb3e3125e578
nicework <aaaaa@gmail.com>
2023-04-13 20:36:54 +0800

    后台路由

--- routes/admin.php
+++ routes/admin.php
@@ -7,7 +7,7 @@ Route::middleware(['admin.sign'])->prefix("hack")->group(function () {
     Route::post("login",[LoginController::class,"login"]);
     Route::middleware(['admin.token'])->group(function (){
         Route::controller(LoginController::class)->group(function () {
-            Route::post("menuList", 'menuList');
+            Route::get("menuList", 'menuList');
         });
     });
 });
