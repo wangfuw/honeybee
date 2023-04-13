c96af7a851ac766cbf880aa8adacf77139bd7a49
nicework <aaaaa@gmail.com>
2023-04-13 18:55:29 +0800

    后台路由

--- app/Http/Middleware/TrimStrings.php
+++ app/Http/Middleware/TrimStrings.php
@@ -13,7 +13,7 @@ class TrimStrings extends Middleware
      */
     protected $except = [
         'current_password',
-//        'password',
-//        'password_confirmation',
+        'password',
+        'password_confirmation',
     ];
 }
