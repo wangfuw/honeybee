26495edf1f7ee453316fc0a69838a43221541ac8
啊实打实多 <358524227@qq.com>
2023-04-13 18:35:57 +0800

    数据连接

--- app/Models/AdminUser.php
+++ app/Models/AdminUser.php
@@ -1,12 +1,29 @@
 <?php
 namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
 
-class AdminUser extends Model
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class AdminUser extends Authenticatable implements JWTSubject
 {
     protected $table = 'admin_user';
 
-    protected $hidden = [
-        'password',
-    ];
+//    protected $hidden = [
+//        'password',
+//    ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
