1f0d3d511b4851bea43e2b46fa324aa521d12942
nicework <wenqinchao@gmail.com>
2023-04-14 23:47:58 +0800

    添加管理员

--- routes/admin.php
+++ routes/admin.php
@@ -39,7 +39,7 @@ Route::middleware(['admin.sign'])->prefix("hack")->group(function () {
     });
 });
 
-Route::middleware(["admin.token"])->group(function (){
+Route::middleware(["admin.token"])->prefix("hack")->group(function (){
     Route::post("upload", [LoginController::class, "uploadMany"]);
 });
 
