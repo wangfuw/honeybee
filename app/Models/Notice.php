418180e7c7bb6ac02e3398f2fe1016dce38a32bb
nicework <wenqinchao@gmail.com>
2023-04-14 22:56:27 +0800

    添加管理员

--- app/Models/Notice.php
+++ app/Models/Notice.php
@@ -4,12 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Notice extends Base
 {
     use HasFactory;
-    use SoftDeletes;
 
     protected $table = 'notice';
 
