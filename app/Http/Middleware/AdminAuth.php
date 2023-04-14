9d74091fa10b5e9dd6a260481365a9e872d3572f
nicework <aaaaa@gmail.com>
2023-04-14 14:13:07 +0800

    后台路由

--- app/Http/Middleware/AdminAuth.php
+++ app/Http/Middleware/AdminAuth.php
@@ -19,7 +19,6 @@ class AdminAuth
     {
         $uri = $request->path();
         $admin = auth("admin")->user();
-        var_dump($admin);
         $rule = AdminRule::where("uri", "/" . $uri)->first();
         $group = AdminGroup::find($admin->group_id);
         $rules = explode(",", $group->rules);
