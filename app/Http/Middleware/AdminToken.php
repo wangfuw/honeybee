438d06bf1a76b85bb68cca1a14e4f09bfb117943
nicework <aaaaa@gmail.com>
2023-04-14 13:31:08 +0800

    后台路由

--- app/Http/Middleware/AdminToken.php
+++ app/Http/Middleware/AdminToken.php
@@ -27,7 +27,7 @@ class AdminToken
             return $this->jsonResponse(ResponseEnum::TOKEN_EXPIRED, '长时间未操作,请重新登录');
         }
         try {
-            $user = auth("admin")->user();
+//            $user = auth("admin")->user();
             $token = auth("admin")->refresh();
             $response = $next($request);
             $response->headers->set('aaa', $token);
