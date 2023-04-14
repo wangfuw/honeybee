5a57d58c72d4bb9e014a09f536b5358cd6624829
nicework <aaaaa@gmail.com>
2023-04-14 20:10:32 +0800

    后台路由

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Banner extends Base
 {
     use HasFactory;
-
-
+    use SoftDeletes;
     protected $fillable = [
         'id',
         'title',
