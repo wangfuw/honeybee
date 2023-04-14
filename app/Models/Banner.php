b59e2dc2f3d23963c3dbda896578b07f3ed22f3b
啊实打实多 <358524227@qq.com>
2023-04-14 16:06:06 +0800

    数据连接

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Banner extends Model
 {
     use HasFactory;
+
+    public function getCreatedATAttribute($value)
+    {
+        if(!is_numeric($value)){
+            return  $value;
+        }
+        return date("Y-m-d h:i:s",$value);
+    }
 }
