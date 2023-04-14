61c12baef67e7ea0cc9db8b53b4c8dd9e949f73d
nicework <aaaaa@gmail.com>
2023-04-14 15:53:45 +0800

    后台路由

---
+++ database/migrations/2023_04_14_075147_create_admin_action_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminActionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin_action', function (Blueprint $table) {
+            $table->id();
+            $table->integer("admin_id")->comment("管理员id");
+            $table->integer("rule_id")->comment("权限id");
+            $table->string("ip")->comment("操作时的ip地址");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin_action');
+    }
+}
