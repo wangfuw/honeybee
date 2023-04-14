84ea9a59e8c7e7bf48b2ab16a01839fcdf0657fc
啊实打实多 <358524227@qq.com>
2023-04-14 21:26:30 +0800

    资讯

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,21 @@ Route::controller(UserController::class)->group(function () {
         Route::post('refresh', 'refresh');
         Route::get('me', 'me');
     });
-
 });
+
+Route::group([],function (){
+    //获取新闻资讯
+    Route::post('news',[NewsController::class,'getNews']);
+    Route::post('news_info',[NewsController::class,'getInfo']);
+    //获取轮播图
+    Route::post('banners',[BannerController::class,'getBanners']);
+    Route::post('create',[BannerController::class,'create']);
+    //获取公告
+    Route::post('notices',[NoticeController::class,'getNotices']);
+    Route::post('notice_info',[NoticeController::class,'getInfo']);
+    Route::group(['middleware'=>'auth'],function (){
+    });
+});
+
+
 
