49d52ff340a8eea1372be7fb9051fdfd75577285
啊实打实多 <358524227@qq.com>
2023-04-14 15:59:30 +0800

    数据连接

--- app/Models/Base.php
+++ app/Models/Base.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Base extends Model
 {
     use HasFactory;
+
+    //关闭自动时间管理 默认值true
+//    public $timestamps = false;
+
+    protected $dateFormat = 'U';
 }
