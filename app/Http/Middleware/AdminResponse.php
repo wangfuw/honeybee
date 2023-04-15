8d4e10b59b5707fd42837a4360886ca58fe63916
nicework <wenqinchao@gmail.com>
2023-04-15 17:28:05 +0800

    添加管理员

--- app/Http/Middleware/AdminResponse.php
+++ app/Http/Middleware/AdminResponse.php
@@ -11,21 +11,21 @@ class AdminResponse
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        if ($request->rule_type == 2 && $response->original["status"] == 1) {
-            $uri = $request->path();
-            $admin = auth("admin")->user();
-            $rule = AdminRule::where("uri", "/" . $uri)->first();
-            $param = $request->all();
-            unset($param["rule_type"]);
-            date_default_timezone_set("Asia/Shanghai");
-            AdminAction::create([
-                "admin_id" => $admin->id,
-                "rule_id" => $rule->id,
-                "param" => json_encode($param),
-                "ip" => $request->ip(),
-                "created_at" => date("Y-m-d H:i:s", time())
-            ]);
-        }
+//        if ($request->rule_type == 2 && $response->original["status"] == 1) {
+//            $uri = $request->path();
+//            $admin = auth("admin")->user();
+//            $rule = AdminRule::where("uri", "/" . $uri)->first();
+//            $param = $request->all();
+//            unset($param["rule_type"]);
+//            date_default_timezone_set("Asia/Shanghai");
+//            AdminAction::create([
+//                "admin_id" => $admin->id,
+//                "rule_id" => $rule->id,
+//                "param" => json_encode($param),
+//                "ip" => $request->ip(),
+//                "created_at" => date("Y-m-d H:i:s", time())
+//            ]);
+//        }
         return $response;
     }
 }
