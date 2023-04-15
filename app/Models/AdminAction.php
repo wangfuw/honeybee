46a32bc4ed0ce3104de288d5cd39d6a90b81c61d
nicework <wenqinchao@gmail.com>
2023-04-15 16:54:35 +0800

    添加管理员

--- app/Models/AdminAction.php
+++ app/Models/AdminAction.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class AdminAction extends Model
 {
     protected $table = 'admin_action';
+
+    protected $fillable = [
+        'id',
+        'admin_id',
+        'rule_id',
+        'ip',
+        'param',
+        'created_at',
+    ];
 }
