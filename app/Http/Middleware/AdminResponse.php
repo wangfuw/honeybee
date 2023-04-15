9d72631ae5a40ced5c1cdf13c6c2dbc46dc93604
nicework <wenqinchao@gmail.com>
2023-04-15 17:01:09 +0800

    添加管理员

--- app/Http/Middleware/AdminResponse.php
+++ app/Http/Middleware/AdminResponse.php
@@ -11,17 +11,18 @@ class AdminResponse
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        if($request->rule_type == 2 && $response->original["status"] == 1){
+        if ($request->rule_type == 2 && $response->original["status"] == 1) {
             $uri = $request->path();
             $admin = auth("admin")->user();
             $rule = AdminRule::where("uri", "/" . $uri)->first();
             $param = $request->all();
             unset($param["rule_type"]);
             AdminAction::create([
-                "admin_id"=>$admin->id,
-                "rule_id"=>$rule->id,
-                "param"=>json_encode($param),
-                "ip"=>$request->ip()
+                "admin_id" => $admin->id,
+                "rule_id" => $rule->id,
+                "param" => json_encode($param),
+                "ip" => $request->ip(),
+                "created_at" => date("Y-m-d H:i:s", time())
             ]);
             var_dump($request->all());
         }
--- app/Models/AdminAction.php
+++ app/Models/AdminAction.php
@@ -8,6 +8,8 @@ class AdminAction extends Model
 {
     protected $table = 'admin_action';
 
+    public $timestamps = false;
+
     protected $fillable = [
         'id',
         'admin_id',
