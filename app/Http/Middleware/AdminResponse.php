1492d7d3b90da5fafb0a2212e5709781b76683a7
nicework <wenqinchao@gmail.com>
2023-04-15 16:50:24 +0800

    添加管理员

--- app/Http/Middleware/AdminResponse.php
+++ app/Http/Middleware/AdminResponse.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\AdminAction;
+use App\Models\AdminRule;
 use Closure;
 
 class AdminResponse
@@ -10,6 +12,17 @@ class AdminResponse
     {
         $response = $next($request);
         if($request->rule_type == 2 && $response->original["status"] == 1){
+            $uri = $request->path();
+            $admin = auth("admin")->user();
+            $rule = AdminRule::where("uri", "/" . $uri)->first();
+            $param = $request->all();
+            unset($param["rule_type"]);
+            AdminAction::create([
+                "admin_id"=>$admin->id,
+                "rule_id"=>$rule->id,
+                "param"=>json_encode($param),
+                "ip"=>$request->ip()
+            ]);
             var_dump($request->all());
         }
         return $response;
