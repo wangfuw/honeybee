c3e301e376e93f0ebe2371661ebb14f20fb7df5d
nicework <aaaaa@gmail.com>
2023-04-20 18:43:12 +0800

    后台区块数据

--- app/Models/AsacDestory.php
+++ app/Models/AsacDestory.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class AsacDestory extends Model
+class AsacDestory extends Base
 {
 
     use HasFactory;
