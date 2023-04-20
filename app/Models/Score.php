1d19077f3b648cf03813e39a423f925cf868a312
啊实打实多 <358524227@qq.com>
2023-04-20 16:11:03 +0800

    通证系列 ，地址管理

--- app/Models/Score.php
+++ app/Models/Score.php
@@ -20,8 +20,20 @@ class Score extends Base
     const BACK_ADD = 1;
     const BACK_SUB = 2;
 
+    const FREE_USED     = 3;
+    const BURN_HAVE     = 4;
+
+    const LUCKY_FREE_USED  = 5;
+    const TRADE_HAVE = 6;
+    const TRADE_USED = 7;
+
     const F_TYPES = [
         self::BACK_ADD => "后台增加",
-        self::BACK_SUB => "后台扣除"
+        self::BACK_SUB => "后台扣除",
+        self::FREE_USED => "释放消耗",
+        self::BURN_HAVE => "燃烧所得",
+        self::LUCKY_FREE_USED => "释放消耗幸运值",
+        self::TRADE_HAVE => "交易得到",
+        self::TRADE_USED => "交易消耗",
     ];
 }
