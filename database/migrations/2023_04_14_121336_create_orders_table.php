ca3e9963a87a5ba96914f548d31885af37a3503d
啊实打实多 <358524227@qq.com>
2023-04-20 22:46:45 +0800

    订单管理

--- database/migrations/2023_04_14_121336_create_orders_table.php
+++ database/migrations/2023_04_14_121336_create_orders_table.php
@@ -15,17 +15,22 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->integer('base_id')->comment('主订单id');
             $table->string('order_no')->comment('订单号');
             $table->integer('user_id')->comment('客户id');
             $table->integer('sku_id')->comment('商品id');
             $table->integer('sku_num')->comment('数量');
-            $table->integer('store_id')->comment('商店id');
+            $table->integer('store_id')->default(0)->comment('0-为自营 商店id');
             $table->tinyInteger('status')->default(1)->comment('1--待支付 2 -- 已支付 3--撤单');
             $table->tinyInteger('express_status')->default(0)->comment('0--待发货 1--已发货 2--签收');
-            $table->tinyInteger('is_return')->default(0)->comment("0-no 1-申请退货 2-申请换货");
+            $table->tinyInteger('is_return')->default(0)->comment("0-no 1-申请换货");
             $table->string('express_no')->default(null)->comment('运单单号');
             $table->string('express_name')->default(null)->comment('快递公司');
             $table->decimal('express_fee',10)->default(0)->comment('运费');
+            $table->decimal('give_green_score',16)->default(0)->comment('获得获得绿色积分');
+            $table->decimal('give_sale_score',16)->default(0)->comment('获得消费积分');
+            $table->decimal('give_lucky_score',16)->default(0)->comment('获得幸运值');
+            $table->decimal('give_ticket_score',16)->default(0)->comment('获得消费卷');
             $table->integer('address_id')->default(0)->comment('收获地址id');
             $table->integer('created_at')->comment('下单时间');
             $table->integer('updated_at')->comment('修改时间');
---
+++ database/migrations/2023_04_20_110131_create_base_order_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBaseOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('base_order', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->comment('用户id');
+            $table->json('products')->comment('商品信息');
+            $table->decimal('money',16,2)->default(0)->comment('订单价格');
+            $table->tinyInteger('type')->comment('订单状态1-待支付 2-已支付');
+            $table->integer('created_at')->comment('插入时间');
+            $table->integer('updated_at')->comment('修改时间');
+            $table->integer('deleted_at')->nullable()->comment('取消时间');
+        });
+        \DB::statement("ALTER TABLE `base_order` comment '主订单表'");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('base_order');
+    }
+}
