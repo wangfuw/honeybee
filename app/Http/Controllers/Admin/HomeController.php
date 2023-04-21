e7cc37fe89e4b00ccae178cc9b37857028911052
nicework <aaaaa@gmail.com>
2023-04-21 22:24:55 +0800

    后台区块数据

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,27 +4,22 @@ namespace App\Http\Controllers\Admin;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends AdminBaseController
 {
     public function registerLine(Request $request)
     {
-        $dates = [];
-        $nums = [];
-
-        $first_user = User::first()->toArray();
-        $start = $first_user["created_at"];
-
-        var_dump($start);
-
-        $last_user = User::orderByDesc("id")->first();
-        $end = $last_user->created_at;
+        $condition = [];
         if ($request->filled("create_at")) {
             $start = $request->input("create_at.0");
             $end = $request->input("create_at.1");
-//            $condition[] = ["created_at", ">=", strtotime($start)];
-//            $condition[] = ["created_at", "<", strtotime($end)];
+            $condition[] = ["created_at", ">=", strtotime($start)];
+            $condition[] = ["created_at", "<", strtotime($end)];
         }
-
+        $data = User::where($condition)->groupBy('date')
+            ->get([DB::raw('DATE(created_at) as date'), DB::raw('COUNT(*) as num')])
+            ->toArray();
+        return $this->executeSuccess("请求", $data);
     }
 }
