943cc501e00fd32be0c0e689d94e8bd242fb2d93
nicework <aaaaa@gmail.com>
2023-04-21 21:17:10 +0800

    后台区块数据

--- app/Http/Middleware/MerchantToken.php
+++ app/Http/Middleware/MerchantToken.php
@@ -26,7 +26,7 @@ class MerchantToken
         }
 
         try {
-            auth("merchant")->user();
+            $user = auth("merchant")->user();
         } catch (\Exception $e) {
             return $this->jsonResponse(ResponseEnum::TOKEN_EXPIRED, '登录令牌失效,请重新登录');
         }
