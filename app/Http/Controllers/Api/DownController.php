85705f12137475392c45108292d1c454827d20be
啊实打实多 <358524227@qq.com>
2023-04-29 14:42:48 +0800

    通证系列 ，地址管理

---
+++ app/Http/Controllers/Api/DownController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\BaseController;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class DownController extends BaseController
+{
+    public function update(Request $request){
+        $version_r = $request->version;
+        $version = DB::table('down')->value('version');
+        if($version_r != $version){
+            $url =  $_SERVER['SERVER_NAME']."storage/app/public/st".$version.".wgt";
+            dd($url);
+        }else{
+            $url = "";
+        }
+        return $this->success("请求成功",['url'=>$url]);
+    }
+}
