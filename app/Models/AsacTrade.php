f8010f4d20da6ab0eb61f4a6c8e3397cd421e9c5
啊实打实多 <358524227@qq.com>
2023-05-02 15:44:35 +0800

    通证系列 ，地址管理

--- app/Models/AsacTrade.php
+++ app/Models/AsacTrade.php
@@ -8,19 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class AsacTrade extends Base
 {
     use HasFactory;
-    //买
+    //买卖花费
     const BUY = 1;
-    //卖
+    //买卖获得
     const SELL = 2;
-    //充值
+    //充值获得
     const RECHARGE = 3;
-    //提现
+    //提现花费
     const WITHDRAW = 4;
-    //奖励
+    //奖励获得
     const REWARD = 5;
-
+    //释放获得
+    const FREE_HAVED = 6;
+    //释放花费
+    const FREE_USED = 7;
     protected $table = 'asac_trade';
 
+    const typeData = [
+        self::BUY => '买卖花费',
+        self::SELL => '买卖获得',
+        self::RECHARGE => '充值获得',
+        self::WITHDRAW => '提现花费',
+        self::REWARD => '奖励获得',
+        self::FREE_HAVED => '释放获得',
+        self::FREE_USED => '释放花费',
+    ];
     protected $fillable = [
         'id','from_address','to_address','num','trade_hash','block_id','created_at','updated_at','type'
     ];
