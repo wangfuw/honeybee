badd23254a0c71dcc87f177f23ebd4cc1cd184bf
nicework <wenqinchao@gmail.com>
2023-05-02 18:45:45 +0800

    配置查看和修改

--- app/Models/MoneyTrade.php
+++ app/Models/MoneyTrade.php
@@ -7,15 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class MoneyTrade extends Base
 {
+    use HasFactory;
+
     protected $table = 'money_trade';
 
     public function fromUser()
     {
-        return $this->hasOne(User::class,'id','from_id');
+        return $this->hasOne(User::class, 'id', 'from_id');
     }
 
     public function toUser()
     {
-        return $this->hasOne(User::class,'id','from_id');
+        return $this->hasOne(User::class, 'id', 'from_id');
     }
 }
