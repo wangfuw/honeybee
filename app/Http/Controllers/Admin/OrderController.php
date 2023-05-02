ca7e62a29b6c3d24b0dffcbebbfbb3851a251252
nicework <wenqinchao@gmail.com>
2023-05-02 18:22:54 +0800

    配置查看和修改

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -73,7 +73,7 @@ class OrderController extends AdminBaseController
         if (!$order) {
             return $this->error("ID");
         }
-        if($order->status != 2){
+        if ($order->status != 2) {
             return $this->fail("订单未支付");
         }
         $order->express_no = $request->express_no;
@@ -114,6 +114,7 @@ class OrderController extends AdminBaseController
         }
         if ($request->express_status != null) {
             $condition[] = ["orders.express_status", "=", $request->express_status];
+            $condition[] = ["order.status", "=", 2];
         }
         if ($request->filled("order_no")) {
             $condition[] = ["orders.order_no", "=", $request->order_no];
