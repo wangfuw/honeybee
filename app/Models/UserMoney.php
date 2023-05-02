28a88c84cdaf0946087a62080f3c38b151368d6c
啊实打实多 <358524227@qq.com>
2023-05-02 16:05:16 +0800

    用户充值余额

---
+++ app/Models/UserMoney.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserMoney extends Base
+{
+    protected $table = 'user_money';
+
+    protected $fillable = [
+
+    ];
+}
