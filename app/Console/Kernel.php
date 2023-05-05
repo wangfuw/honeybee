c5e3aae9276f3f301ef1c0c28054c477c6bda6a4
啊实打实多 <358524227@qq.com>
2023-05-05 21:30:23 +0800

    币种信息

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('free_score')
-            ->daily()
+            ->everyTenMinutes()
             ->withoutOverlapping() //防止重复执行
             ->onOneServer() //在单台服务器上跑
             ->runInBackground() //任务后台运行
@@ -39,7 +39,7 @@ class Kernel extends ConsoleKernel
             //->appendOutputTo('log_path')//日志输出，默认追加
             ->sendOutputTo('log_path'); //日志输出，默认覆盖先前日志;
         //释放冻结余额
-        $schedule->command('free_money')->daily()
+        $schedule->command('free_money')->everyTenMinutes()
             ->withoutOverlapping() //防止重复执行
             ->onOneServer() //在单台服务器上跑
             ->runInBackground() //任务后台运行
