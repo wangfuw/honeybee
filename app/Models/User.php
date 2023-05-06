1385afc6f32f1e111cec30e59107332a87918723
啊实打实多 <358524227@qq.com>
2023-05-07 02:36:40 +0800

    签收

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,7 +23,7 @@ class User extends Authenticatable implements JWTSubject
     'id','nickname','phone','is_shop','image','green_score','sale_score',
         'luck_score','coin_num','invite_code','master_id','master_pos','password',
         'created_at','updated_at','sale_password','ticket_num','contribution',
-        'identity_area','identity_area_code','identity_status','max_luck_num'
+        'identity_area','identity_area_code','identity_status','max_luck_num','money','freeze_money','new_freeze'
     ];
 
     /**
