50599c5d8a7fd461e7bf8ab67fb97ae470c326f2
nicework <aaaaa@gmail.com>
2023-05-08 13:32:04 +0800

    后台区块数据

--- app/Models/AsacNode.php
+++ app/Models/AsacNode.php
@@ -35,12 +35,12 @@ class AsacNode extends Base
                 if($item->id <= 4){
                     $item->money = bcmul($item->number,$config['last_price']);
                     $temp = bcdiv($item->number * 100,$config['number'],2);
-                    $item['ratio'] = number_format($temp,2).'%';
+                    $item ->ratio = number_format($temp,2).'%';
                 }else{
                     $item->number = $item->user->coin_num;
                     $item->money = bcmul($item->user->coin_num??0,$config['last_price'])??0;
                     $temp = bcdiv($item->user->coin_num??0 * 100,$config['number'],2)??0;
-                    $item['ratio'] = number_format($temp,2).'%'??0;
+                    $item ->ratio = number_format($temp,2).'%'??0;
                 }
 //                unset($temp,$item->user);
                 return $item;
