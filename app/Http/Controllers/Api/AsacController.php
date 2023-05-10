84fd6b05a57f54da49b198a62e008a61abadbcf3
啊实打实多 <358524227@qq.com>
2023-05-10 22:39:33 +0800

    tets

--- app/Http/Controllers/Api/AsacController.php
+++ app/Http/Controllers/Api/AsacController.php
@@ -191,7 +191,7 @@ class AsacController extends BaseController
     //流动池 预挖池 流转记录
     public function get_flue(Request $request)
     {
-        $type = $request->post('type'); //1 流动池子 2 预挖池
+        $type = $request->input('type'); //1 流动池子 2 预挖池
         $page = $request->page??1;
         $page_size = $request->page_size??6;
         if($type == 1){
