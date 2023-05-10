0d097ba0e0923b7cd3da02b1eb3440b648207723
啊实打实多 <358524227@qq.com>
2023-05-10 14:27:42 +0800

    签收

--- app/Http/Controllers/Api/DownController.php
+++ app/Http/Controllers/Api/DownController.php
@@ -11,9 +11,9 @@ use Illuminate\Support\Facades\DB;
 class DownController extends BaseController
 {
     public function update(Request $request){
-        echo Rsa::publicKeyEncode('13212345622');
+        dd(Rsa::publicKeyEncode('13212345622'));
         echo Rsa::publicKeyEncode('123456');
-        dd(123);
+
         $version_r = $request->version;
         $version = DB::table('down')->value('version');
         if($version_r != $version){
