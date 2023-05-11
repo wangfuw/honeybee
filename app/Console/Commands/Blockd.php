1163e9b30054c43b07cad77021175280b72bdd85
啊实打实多 <358524227@qq.com>
2023-05-11 16:54:40 +0800

    tets

--- app/Console/Commands/Blockd.php
+++ app/Console/Commands/Blockd.php
@@ -56,7 +56,7 @@ class Blockd extends Command
                     'trade_num'=> count($list),
                     'number'   => 0,
                 ]);
-                AsacTrade::query()->whereIn('id',$list)->update(['block_id'=>$res->id]);
+                AsacTrade::query()->whereIn('id',$list->toArray())->update(['block_id'=>$res->id]);
                 DB::commit();
                 Log::info('打包结束'.date('Y-m-d H:i:s'));
                 return false;
