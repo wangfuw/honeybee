7d4a3ed9d6d092b5804b1023d0356cd75e23b958
啊实打实多 <358524227@qq.com>
2023-06-15 14:46:52 +0800

    关于1账号数量不正确 修改

--- app/Http/Controllers/Admin/ScoreController.php
+++ app/Http/Controllers/Admin/ScoreController.php
@@ -120,8 +120,8 @@ class ScoreController extends AdminBaseController
             foreach ($data as &$value){
                 $user_id_from = AsacNode::query()->where('wallet_address',$value['from_address'])->value('user_id');
                 $user_id_to = AsacNode::query()->where('wallet_address',$value['to_address'])->value('user_id');
-                $value['from_phone'] = User::query()->where('id',$user_id_from)->value('phone');
-                $value['to_phone'] = User::query()->where('id',$user_id_to)->value('phone');
+                $value['from_phone'] = User::query()->where('id',$user_id_from)->value('phone')??'--';
+                $value['to_phone'] = User::query()->where('id',$user_id_to)->value('phone')??'--';
             }
         }
         return $this->executeSuccess("request", $data);
