dc02bebc719bd515d6f8195f5095577fbc62c28b
啊实打实多 <358524227@qq.com>
2023-06-28 12:12:40 +0800

    关于1账号数量不正确 修改

--- app/Http/Controllers/Api/PayController.php
+++ app/Http/Controllers/Api/PayController.php
@@ -7,6 +7,7 @@ use App\Models\AsacNode;
 use App\Models\Config;
 use App\Models\Score;
 use App\Models\Store;
+use App\Models\StoreSupply;
 use App\Models\User;
 use App\Validate\PayValidate;
 use Illuminate\Http\Request;
@@ -15,23 +16,34 @@ use Illuminate\Support\Facades\Hash;
 
 class PayController extends BaseController
 {
+    //微信appid
     const WX_APPID="wx1f331c16c0da4ede";
+    //微信密钥
     const WX_SECRET="3b5c0e3bd111f8ab14d338b3e67140d0";
 
     const VERSION = '2.2';
+
+    //平台编号
     const MERCHANTNO = '888120600004799';
 
+    //报备商户号
     const TRADEMERCHANTNO = "777190600517604";
-
+   //报备商户号 备用
     const TRADEMERCHANTNO_BAT = "777163800517605";
 
+    const CUR = 1; //人名币
+    const CAI = 1; //1分账
+    const CAI_TYPE = 11; //时时分账
+    //md5 密钥
     const M_SECRET = '82315039593446e3a81d61e71dfdac99';
 
+    //分配率
     const QE_RATE = 0.7;
     const PAY_APPID="";
 
     const PAY_SECRET="";
 
+    //支付url
     const URL = "https://www.joinpay.com/trade/uniPayApi.action";
 
     private $down_url;
@@ -39,7 +51,6 @@ class PayController extends BaseController
     {
         $this->validate = $validate;
         $this->middleware('auth:api', ['except' => ['to_pey','notify_url']]);
-        $this->down_url = DB::table('down')->where('id',1)->value('android');
     }
 
     //输入电话 金额 选着支付方式 调取api/to_pay （注册用户，异步生成预支付订单）返回订单信息 拉去微信或支付宝jsapi支付，支付成功给用户加消费积分，商家加积分
@@ -52,73 +63,137 @@ class PayController extends BaseController
         if(!check_phone($data['phone'])){
             return $this->fail('电话号码错误');
         }
-        if($data['pay_type'] == 'wx_pay'){
-            //获取用户openid
-            $info = curl_get(self::WX_APPID,self::WX_SECRET,$data['code'],'authorization_code','GET');
-            //根据用户id 获取用户商户编号
-            $store_info = Store::query()->where('user_id',$data['id'])->first();
-            //注册用户，默认密码是123456
-            $this->auto_register($data);
-            //调汇聚接口生成预支付订单
-            $paramData = $this->pre_data($data,$info['openid'],$store_info->mch_no);
-            $paramString = $this->pre_data($data,$info['openid'],$store_info->mch_no);
-            $hashSecret = hmacRequest($paramString,self::M_SECRET,1);
-            $paramData['hmac'] = $hashSecret;
-            $ret = post_url(self::URL,$paramData);
-            dd(json_decode($ret,true));
-            //预支付信息返回前端
-            return $this->success('请求成功',$ret['rc_Result']);
-        }else{
-            //支付宝支付
+        try {
+            DB::beginTransaction();
+            if($data['pay_type'] == 'wx_pay'){
+                //获取用户openid
+                $info = curl_get(self::WX_APPID,self::WX_SECRET,$data['code'],'authorization_code','GET');
+                dd($info);
+                //根据用户id 获取用户商户编号
+                $store_info = StoreSupply::query()->where('user_id',$data['id'])->first();
+                //注册用户，默认密码是123456
+               // $ret = $this->auto_register($data);
+                //调汇聚接口生成预支付订单
+                [$data,$sign] = $this->pre_data($data,$info['openid'],$store_info->alt_mch_no);
+                //[$data,$sign] = $this->pre_data($data,$openid,$store_info->alt_mch_no);
+                unset($data['key']);
+                $data['hmac'] = $sign;
+                $ret = post_url(self::URL,$data);
+                //预支付信息返回前端
+                return $this->success('请求成功',$ret['rc_Result']);
+            }else{
+                //支付宝支付
+
+            }
+            DB::commit();
+            return $this->success('请求成功',[]);
+        }catch (\Exception $e){
 
         }
 
-        return $this->success('请求成功',[]);
     }
 
+    //wx - 支付参数
+    protected function pre_data($data,$openid,$mch_no)
+    {
+        $data =  [
+            'p0_Version'=>self::VERSION,
+            'p1_MerchantNo'=>self::MERCHANTNO,
+            'p2_OrderNo'=>generateOrderNumber(),
+            'p3_Amount'=>$data['money'],
+            'p4_Cur'=>self::CUR,
+            'p5_ProductName'=>'源宇通下线商品',
+            //服务器异步通知地址
+            'p9_NotifyUrl'=>env("CALL_BACK","http://api.yuanyutong.shop").'/api/notify_url',
+            'q1_FrpCode'=>'WEIXIN_GZH',
+            'q5_OpenId'=>$openid,
+            'q7_AppId'=>self::WX_APPID,
+            'qa_TradeMerchantNo'=>self::TRADEMERCHANTNO,
+            'qc_IsAlt'=>self::CAI,
+            'qd_AltType'=>self::CAI_TYPE,
+            'qe_AltInfo'=>[
+                [
+                    'altMchNo'=>$mch_no,"altAmount"=>bcmul($data['money'],
+                    self::QE_RATE)
+                ]
+            ],
+            //实时分账在支付完成后，分账信息会异步通知商户结果，回调 qf_AltUrl 中的地址
+            'qf_AltUrl'=>env("CALL_BACK","http://api.yuanyutong.shop").'/api/qf_alt_url',
+            'qi_FqSellerPercen' => 0,
+            'key'=> self::M_SECRET
+        ];
+
+        return $this->sign_str($data);
+    }
+    //加密
+    private function sign_str($data)
+    {
+        $str ='';
+        foreach ($data as $key=>$value){
+            if($key=='qe_AltInfo'){
+                $value = json_encode($value,JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+            }
+            $str.="$value";
+        }
+        $str = rtrim($str,'');
+        return [$data,md5($str)];
+    }
+
+    //服务器异步通知地址
+    public function notify_url(Request $request)
+    {
+
+    }
+    //分账通知地
+    public function qf_alt_url(Request $request)
+    {
+
+    }
     //自动注册
-    protected function auto_register($data)
+    protected function auto_register($data):void
     {
         $s_users = User::query()->where('id',$data['id'])->first();
+
         $user = User::query()->where('phone',$data['phone'])->first();
-        if($user){
-            return false;
+        if(!$user){
+            $myself_invite_code = inviteCode($data['phone']);
+            $num = Config::register_give_lucky();
+            $d = [
+                'phone' => $data['phone'],
+                'invite_code'=> $myself_invite_code,
+                //初始密码123456
+                'password'=>Hash::make('123456'),
+                'master_id' => $s_users->id,
+                'master_pos'=>','.$user->id.$s_users->master_pos.','??'',
+                'luck_score'=>$num??180,
+                'max_luck_num'=>$num??180,
+                'sale_password'=>'123456'
+            ];
+            $user = User::create($d);
+            //注册赠送幸运值
+            Score::query()->create([
+                "user_id"=>$user->id,
+                "flag"   => 1,
+                "num"    =>$num??180,
+                "type"=>3,
+                "f_type"=>Score::REGISTER_REWARD
+            ]);
+            AsacNode::create([
+                'user_id' => $user->id,
+                'wallet_address' => rand_str_pay(40),
+                'private_key' => rand_str_pay(64)
+            ]);
+            $content = "【源宇通商城】您已注册源宇通商城。您的账户是:".$data['phone']."您的登录初始密码是:123456。";
+            send_sms($data['phone'],$content);
         }
-        $myself_invite_code = inviteCode($data['phone']);
-        $num = Config::register_give_lucky();
-        $user = User::query()->create([
-            'phone' => $data['phone'],
-            'invite_code'=> $myself_invite_code,
-            //初始密码123456
-            'password'=>Hash::make('123456'),
-            'master_id' => $s_users->id,
-            'master_pos'=>','.$user->id.$s_users->master_pos??'',
-            'luck_score'=>$num??180,
-            'max_luck_num'=>$num??180,
-            'sale_password'=>'123456'
-        ]);
-        //注册赠送幸运值
-        Score::query()->create([
-            "user_id"=>$user->id,
-            "flag"   => 1,
-            "num"    =>$num??180,
-            "type"=>3,
-            "f_type"=>Score::REGISTER_REWARD
-        ]);
-        AsacNode::create([
-            'user_id' => $user->id,
-            'wallet_address' => rand_str_pay(40),
-            'private_key' => rand_str_pay(64)
-        ]);
-        $this->send_register($data['phone'],$this->down_url);
-        return true;
+
     }
 
     //发送注册信息--todo
-    protected function send_register($phone,$down_url)
+    protected function send_register($phone)
     {
-        $content = "【源宇通商城】您已注册源宇通商城。您的账户是:".$phone."您的登录初始密码是:123456。"."APP下安装地址是:".$down_url;
-        send_sms($phone,$content);
+
+
     }
     //生成预支付订单
     protected function make_pre_order($data,$openid)
@@ -126,36 +201,9 @@ class PayController extends BaseController
 
     }
 
-    //支付成功回调 给用户增加消费积分，给商户增加消费积分
+    //支付成功回调
     public function pay_order_back()
     {
 
     }
-
-    protected function pre_data($data,$openid,$mch_no)
-    {
-        return [
-            'p0_Version'=>self::VERSION,
-            'p1_MerchantNo'=>self::MERCHANTNO,
-            'p2_OrderNo'=>generateOrderNumber(),
-            'p3_Amount'=>$data['money'],
-            'p4_Cur'=>'1',
-            'p5_ProductName'=>'源宇通商品',
-            'p9_NotifyUrl'=>'/api/notify_url',
-            'q1_FrpCode'=>'WEIXIN_GZH',
-            'q5_OpenId'=>$openid,
-            'q7_AppId'=>self::WX_APPID,
-            'qa_TradeMerchantNo'=>self::TRADEMERCHANTNO,
-            'qc_IsAlt'=>1,
-            'qd_AltType'=>11,
-            'qe_AltInfo'=>json_encode([['altMchNo'=>$mch_no,"altAmount"=>bcmul($data['money'],self::QE_RATE)]]),
-            //分账通知地址'qf_AltUrl'=>
-        ];
-    }
-
-    //系统通知地址
-    public function notify_url(Request $request){
-
-    }
-
 }
