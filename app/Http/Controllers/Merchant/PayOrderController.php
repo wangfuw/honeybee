5bf87567415853243d5f3d463f5f2ae6954ff830
啊实打实多 <358524227@qq.com>
2023-06-29 18:21:02 +0800

    关于1账号数量不正确 修改

--- app/Http/Controllers/Merchant/PayOrderController.php
+++ app/Http/Controllers/Merchant/PayOrderController.php
@@ -21,8 +21,12 @@ class PayOrderController extends MerchantBaseController
         $alt_mch_no = StoreSupply::query()->where('user_id',$user->id)->value('alt_mch_no');
         foreach ($data["data"] as $k => &$v) {
             $v["alt_mch_no"] = $alt_mch_no;
+            if($v['alt_info']){
+                $temp = explode('|',$v['alt_info']);
+                $f = explode('-',$temp[1]);
+                $v["money"] = $f["2"];
+            }
         }
         return $this->executeSuccess("请求", $data);
     }
-
 }
