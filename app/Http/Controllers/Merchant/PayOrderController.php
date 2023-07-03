61c356a9f4e60e942e039dcb47849c9b77daf6fe
啊实打实多 <358524227@qq.com>
2023-07-03 18:00:12 +0800

    关于1账号数量不正确 修改

--- app/Http/Controllers/Merchant/PayOrderController.php
+++ app/Http/Controllers/Merchant/PayOrderController.php
@@ -30,7 +30,6 @@ class PayOrderController extends MerchantBaseController
             ->paginate($size)
             ->toArray();
         [$alt_mch_no,$all_money,$out_money] = $this->getCashInfo($user->id);
-        return $this->success(123,[$alt_mch_no,$all_money,$out_money]);
         foreach ($data["data"] as $k => &$v) {
             $v["alt_mch_no"] = $alt_mch_no;
             if($v['alt_info']){
@@ -59,7 +58,7 @@ class PayOrderController extends MerchantBaseController
                 $all_money += $f["2"];
             }
         }
-        return compact('alt_mch_no','all_money','out_money');
+        return [$alt_mch_no,$all_money,$out_money];
     }
     public function outCashList(Request $request)
     {
