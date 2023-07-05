633ddeb04ee145887d689af5d1077e4dcbb5f147
啊实打实多 <358524227@qq.com>
2023-07-05 16:45:04 +0800

    关于1账号数量不正确 修改

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -69,7 +69,7 @@ class Store extends Base
         if(!$list) return [];
         $new = [];
         foreach ($list as $l){
-            if(getdistance($longitude,$latitude,$l->longitude,$l->latitude) < 30000){
+            if(getdistance($longitude,$latitude,$l->longitude,$l->latitude) < 500000){
                 $distance = getdistance($longitude,$latitude,$l->longitude,$l->latitude);
                 $l->distance = bcdiv($distance,1000,2);
                 $l->area_china = city_name($l->area);
